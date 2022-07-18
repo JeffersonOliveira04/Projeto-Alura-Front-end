5f4d046aa18abdc38ccb7537d3fad9eb0637eb66
natyprys <jeffegolgol@gmail.com>
2022-07-18 13:37:02 -0300

    projeto-alura

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,29 +1,18 @@
 function tocaSom (idElemetoAudio) {
-
     document.querySelector(idElemetoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-//enquanto
-while (contador < listaDeTeclas.length) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
     const idAudio = `#som_${instrumento}`;
     
-    //console.log(idAudio);
-
-
    tecla.onclick = function () {
         tocaSom(idAudio);
    }
-
-   contador = contador + 1;
-
-    console.log(contador);
+  
 }
-
